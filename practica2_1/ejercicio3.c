995ff03cac29fffd758a417d7f7467dc0b60b886
Arkh3 <ateruelfe@gmail.com>
2020-12-01 11:06:11 +0100

    ejercicio 3 con variante

--- practica2_1/ejercicio3.c
+++ practica2_1/ejercicio3.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+//execl
+#include <stdlib.h>
+
 #define MAX_ERRORES 255
 
 
@@ -9,7 +12,11 @@ int main(){
 	
 	for(int i = 0; i < MAX_ERRORES; ++i)
 		printf("El mensaje asociado al error '%d' es: %s.\n", i, strerror(i));
-	
+
+	/*puedes comentar la parte de arriba y descomentar la linea de abajo pero para poder
+	ejecutar el comando "errno -l" necesitas el paquete moreutils*/
+	//system("errno -l");
+		
 	return 0;
 }
 
